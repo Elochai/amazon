0a1c6840d60a45e5f723af14b1b06ebf5b78b6c7
Eko <eko@eko-lenovo.(none)>
2014-02-07 11:08:03 +0200

    configuring database-cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,8 +31,12 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.start
   end
 
   config.before(:each) do
